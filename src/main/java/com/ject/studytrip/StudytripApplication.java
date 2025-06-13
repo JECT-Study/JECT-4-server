9ce13b1d70076c10eec858baf83d89d3ac87b431
songhyeonpk <ehftozl1997@gmail.com>
2025-06-13 16:20:13 +0900

    init: project initial
    * chore: Gradle 기반 프로젝트 초기화
    * chore: 주요 dependency 등록
    * chore: *.env .gitignore 등록
    * chore: 코드 포맷 툴(spotless) 적용 및 pre-commit script 세팅
    * chore: application.properties -> application.yml 전환
    * chore: 환경별 설정 분리 (test, local, dev, prod, datasource, redis, security, storage)

---
+++ src/main/java/com/ject/studytrip/StudytripApplication.java
@@ -0,0 +1,13 @@
+package com.ject.studytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudytripApplication.class, args);
+    }
+
+}
