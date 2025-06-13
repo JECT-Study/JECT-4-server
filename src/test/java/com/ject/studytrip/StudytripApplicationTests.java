5d4dc9a7c67130d7dd2cdcdcc0c8a722061b1b5f
songhyeonpk <ehftozl1997@gmail.com>
2025-06-13 16:20:13 +0900

    init: project initial

---
+++ src/test/java/com/ject/studytrip/StudytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ject.studytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudytripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
