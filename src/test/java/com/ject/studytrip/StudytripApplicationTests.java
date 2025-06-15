6dc1d140dd12e7abb7e3212d0ac703b36d47501a
songhyeonpk <ehftozl1997@gmail.com>
2025-06-15 17:25:43 +0900

    chore: 프로젝트 세팅 (#1)
    * chore: 이슈 템플릿 세팅 (bug, chore, documentation, feature, refactor)
    * chore: PR 템플릿 세팅
    * chore: .gitignore .DS_store 파일 등록
    * chore: 주석 처리된 Flyway, AWS S3 dependency 및 관련 설정 제거 (추후 필요 시 다시 추가 예정)
    * chore: 환경별 설정 세팅
    * chore: develop pull request 워크플로우 구성
    * chore: Test 클래스에 active profile, auto configure test database 설정 세팅

--- src/test/java/com/ject/studytrip/StudytripApplicationTests.java
+++ src/test/java/com/ject/studytrip/StudytripApplicationTests.java
@@ -1,13 +1,18 @@
 package com.ject.studytrip;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(
+        replace = AutoConfigureTestDatabase.Replace.NONE) // 테스트 시 내장된 인메모리 DB를 사용하지 않는다는 설정
 @SpringBootTest
 class StudytripApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("🌐 DB_HOST = " + System.getenv("DB_HOST"));
     }
-
 }
