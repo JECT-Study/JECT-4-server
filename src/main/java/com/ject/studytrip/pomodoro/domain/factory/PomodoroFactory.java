c7ac7823a1fd4862518ed60bfe23c6a0904104e0
songhyeonpk <ehftozl1997@gmail.com>
2025-07-21 18:10:40 +0900

    feat: 뽀모도로 엔티티 구현(#33)
    * feat: PomodoroFactory 클래스에 엔티티를 생성하는 create() 메서드 추가

---
+++ src/main/java/com/ject/studytrip/pomodoro/domain/factory/PomodoroFactory.java
@@ -0,0 +1,17 @@
+package com.ject.studytrip.pomodoro.domain.factory;
+
+import com.ject.studytrip.pomodoro.domain.model.Pomodoro;
+import com.ject.studytrip.trip.domain.model.DailyGoal;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PomodoroFactory {
+    public static Pomodoro create(
+            DailyGoal dailyGoal,
+            int focusDurationInSeconds,
+            int focusCount,
+            int breakDurationInSeconds) {
+        return Pomodoro.of(dailyGoal, focusDurationInSeconds, focusCount, breakDurationInSeconds);
+    }
+}
